19a02b1aa76475a9f51e3e03f9fa45fe9a8ce708
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2022-08-21 09:19:31 +0530

    Add brotli

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+import { defineNuxtConfig } from 'nuxt'
+import viteCompression from 'vite-plugin-compression';
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    vite: {
+      plugins: [viteCompression({ algorithm: 'brotliCompress' })]
+    }
+  });
